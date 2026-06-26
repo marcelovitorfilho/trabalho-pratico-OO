51c177c62d98c88c449e88cd9e671e89f75c67e4
Brunovieira-10 <contatobrunova@gmail.com>
2026-06-26 17:11:32 -0300

    Main Vendas Bruno

---
+++ br/edu/cafeteria /app/Main.java
@@ -0,0 +1,38 @@
+package br.edu.cafeteria.modelo;
+
+import br.edu.cafeteria.excecao.EstoqueInsuficienteException;
+import br.edu.cafeteria.excecao.PontosInsuficientesException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pedido {
+    //============================================
+    // SISTEMA DE VENDAS - Cliente casual 
+    // ===========================================
+    
+    private static int NumeroIdentificacao = 1000; // gerador sequencial automatico exigido
+    private String atendente;
+    private Cliente cliente; // pode ser Casual, Standard ou VIP
+    private List<itemPedido> itens = new ArrayList<itemPedido>();
+    private double descontoManual;
+
+    public Pedido(int numeroIdentificacao, String atendente , Cliente cliente){
+        this.NumeroIdentificacao = numeroIdentificacao;
+        this.atendente = atendente;
+        this.cliente = cliente;
+
+    }
+    public void adicionarProduto(Produto p) throws EstoqueInsuficienteException{ // sobrecarga
+        adicionarProduto(p, 1);
+        }
+    public void adicionarProduto(Produto p, int quantidade) throws EstoqueInsuficienteException {
+        if(p.getQuantidadeestoque() < quantidade){
+            throw new EstoqueInsuficienteException("EStoque insuficiente para " + p.getNome());
+        }
+        itens.add(new itemPedido(p,quantidade));
+        
+        public 
+    }
+
+}
+    
