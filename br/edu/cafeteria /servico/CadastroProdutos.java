48e5d52fdbcaa8b8ca58360283473d39ac19e0ce
Lucas Renato <92279826+renato-matias@users.noreply.github.com>
2026-06-26 17:30:30 -0300

    excecaocommit

---
+++ br/edu/cafeteria /servico/CadastroProdutos.java
@@ -0,0 +1,12 @@
+package br.edu.cafeteria.servico;
+
+import java.util.ArrayList;
+import br.edu.cafeteria.modelo.Produto;
+
+public class CadastroProdutos {
+    private ArrayList<Produto> produtos;
+
+    public CadastroProdutos() {
+        this.produtos = new ArrayList<>();
+    }
+}
