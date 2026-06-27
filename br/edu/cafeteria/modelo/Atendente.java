0fc684d49b8fd46e9263a70bb20254dfefa41f88
Lucas Renato <92279826+renato-matias@users.noreply.github.com>
2026-06-27 19:44:38 -0300

    correçãoatendente

--- br/edu/cafeteria/modelo/Atendente.java
+++ br/edu/cafeteria/modelo/Atendente.java
@@ -2,18 +2,18 @@ package br.edu.cafeteria.modelo;
 
 public class Atendente {
     private String nome;
-    private String registro_de_funcionario;
+    private String registroFuncionario;
 
-    public Atendente(String nome, String cpf) {
-    this.nome = nome;
-    this.registro_de_funcionario = registro_de_funcionario;
+    public Atendente(String nome, String registroFuncionario) {
+        this.nome = nome;
+        this.registroFuncionario = registroFuncionario;
     }
 
     public String getNome() {
         return nome;
     }
 
-    public String getRegistro_de_funcionario() {
-        return registro_de_funcionario;
+    public String getRegistroFuncionario() {
+        return registroFuncionario;
     }
 }
