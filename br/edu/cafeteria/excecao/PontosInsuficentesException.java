72366e7eee6e5a71dd182c91025bb332ecbd11f5
Lucas Renato <92279826+renato-matias@users.noreply.github.com>
2026-06-27 18:12:10 -0300

    correcao nome pontos insuficientes

--- br/edu/cafeteria/excecao/PontosInsuficentesException.java
+++
@@ -1,8 +0,0 @@
-package br.edu.cafeteria.excecao;
-
-public class PontosInsuficienteException extends Exception {
-
- public PontosInsuficienteException(String resposta) {
-       super(mensagem);
-    }
-}
