02e73a5354f2b44f3e1ba7bf724b4a2f2b3cf8cd
Marcelo Vitor Machado da Silva Filho <marcelovitorfilho@hotmail.com>
2026-06-27 11:12:49 -0300

    ajuste na Main e no pedido

--- br/edu/cafeteria /app/Main.java
+++ br/edu/cafeteria /app/Main.java
@@ -5,66 +5,9 @@ import br.edu.cafeteria.excecao.PontosInsuficientesException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Pedido {
-    //============================================
-    // SISTEMA DE VENDAS - Cliente casual 
-    // ===========================================
-    
-    private static int NumeroIdentificacao = 1000; // gerador sequencial automatico exigido
-    private String atendente;
-    private Cliente cliente; // pode ser Casual, Standard ou VIP
-    private List<itemPedido> itens = new ArrayList<itemPedido>();
-    private double descontoManual;
+public class Main {
 
-    public Pedido(int numeroIdentificacao, String atendente , Cliente cliente){
-        this.NumeroIdentificacao = numeroIdentificacao;
-        this.atendente = atendente;
-        this.cliente = cliente;
-
-    }
-    public void adicionarProduto(Produto p) throws EstoqueInsuficienteException{ // sobrecarga
-        adicionarProduto(p, 1);
-        }
-    public void adicionarProduto(Produto p, int quantidade) throws EstoqueInsuficienteException {
-        if(p.getQuantidadeestoque() < quantidade){
-            throw new EstoqueInsuficienteException("EStoque insuficiente para " + p.getNome());
-        }
-        itens.add(new itemPedido(p,quantidade));
-    }
-        public double calcularValorTotal(){
-            double total = 0.0;
-            for (itemPedido item : itens){
-                total += itemPedido.getSubtotal();
-            }
-            return Math.max(0.0, total - descontoManual);
-        }
-        public void finalizarPedido(boolean UsarXpComoPagamento) throws EstoqueInsuficienteException, PontosInsuficientesException{
-            double valorFinal = calcularValorFinal();
-
-            if (UsarXpComoPagamento){
-                if (cliente != null){
-                    cliente.pagarComXp(valorFinal);
-                }else{
-                    throw new PontosInsuficientesException("Clientes casuais nao podem usar o beneficio de XP ");
-                }
-            }
-            for (itemPedido item : itens) {
-                item.getProduto().baixarEstoque(item.getQuantidade());
-        }
-    if (!usarXpComoPagamento && cliente != null) {
-            cliente.calcularEAdicionarXP(valorFinal);
-        }
-    }
-    public void aplicarDescontoManual(double valor) {
-        this.descontoManual = valor;
-    }
-    public List<ItemPedido> getItens(){ 
-        return this.itens; 
-    }
-    public int getNumeroIdentificacao(){ 
-        return this.numeroIdentificacao; 
-    }
-        }
+}
     
 
 
