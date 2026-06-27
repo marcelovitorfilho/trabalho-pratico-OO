3a89cf8b42d347b2f6fdcd348f7f0f3a5540dff7
Lucas Renato <92279826+renato-matias@users.noreply.github.com>
2026-06-27 11:30:31 -0300

    cadastroprodutos

--- br/edu/cafeteria /servico/CadastroProdutos.java
+++ br/edu/cafeteria /servico/CadastroProdutos.java
@@ -9,4 +9,43 @@ public class CadastroProdutos {
     public CadastroProdutos() {
         this.produtos = new ArrayList<>();
     }
+    //CREATE//
+    public void cadastrarProduto(Produto produto) {
+        if (buscarProdutoPorCodigo(produto.getCodigo()) != null) {
+            throw new IllegalArgumentException("Já existe um produto cadastrado com esse código.");
+        }
+
+        produtos.add(produto);
+    }
+    //READ//
+    public Produto buscarProdutoPorCodigo(String codigo){
+        for (Produto produto : produtos) {
+            if (produto.getCodigo().equals(codigo)) {
+                return produto;
+        }
+
+        return null;
+    }
+    //DELETE//
+    public void removerProdutoPorCodigo(String codigo){
+        Produto produto = buscarProdutoPorCodigo(codigo);
+
+        if (produto == null) {
+            throw new IllegalArgumentException("Produto não encontrado.");
+        }
+
+        produtos.remove(produto);
+        }
+    }
+    //UPDATE//
+    public void atualizarProduto(String codigo, Produto produtoAtualizado){
+        for (int i = 0; i < produtos.size(); i++) {
+            if (produtos.get(i).getCodigo().equals(codigo)) {
+                produtos.set(i, produtoAtualizado);
+                return;
+            }
+        }
+
+        throw new IllegalArgumentException("Produto não encontrado.");
+    }
 }
