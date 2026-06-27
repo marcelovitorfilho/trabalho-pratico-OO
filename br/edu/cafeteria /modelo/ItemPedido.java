4238504ff6fecd2f39ccf5c47d477f2387a95acb
Brunovieira-10 <contatobrunova@gmail.com>
2026-06-27 12:40:07 -0300

    item.Pedido com IllegalException

---
+++ br/edu/cafeteria /modelo/ItemPedido.java
@@ -0,0 +1,37 @@
+package br.edu.cafeteria.modelo;
+
+public class item.Pedido {
+        private final Produto produto;
+        private int qtd; // quantidade de itens pedidos pelo cliente
+
+        public itemPedido(Produto produto, int qtd){
+            
+            if(produto == null){
+                throw new IllegalArgumentException("O produto não pode ser igual a 0");
+            }
+            if (qtd <= 0){
+                throw new IllegalArgumentException("A quantidade deve ser maior que 0");
+            }
+            this.produto = produto;
+            this.qtd = qtd;
+        }
+        public double getSubtotal() {
+            return produto.getPrecoBase() * qtd;
+        }
+        public void setQuantidade(int quantidade){
+            if(qtd <= 0){
+                throw new IllegalArgumentException("A quantidade deve ser maior do que 0.");
+            }
+            this.qtd = qtd;
+        }
+        public Produto get.Produto(){
+            return produto;
+        }
+        public int getQtd(){
+            return qtd;
+        }
+        @Override // redefinindo um método que ja existe em uma classe mãe
+        public String toString(){
+            return String.format("%s x %d | Subtotal: R$ %.2f", produto.getNome(), quantidade, getSubtotal());
+    }
+}
